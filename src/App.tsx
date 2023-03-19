44d6f296269808173028623cc7fae9a0dda91a8c
utof0x <popovychmykhailo00@gmail.com>
2023-03-19 12:42:37 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/home/Home";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
